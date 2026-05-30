026f11c4466be8b6accc617eb0b5fb707b754c77
arenjiee <moskovwtf@gmail.com>
2026-05-30 16:46:34 +0800

    UPDATE: setting gradle dan menambahkan Main

---
+++ app/src/main/java/ThreadHub/Main.java
@@ -0,0 +1,17 @@
+package ThreadHub;
+
+import ThreadHub.view.LoginView;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        new LoginView(primaryStage).show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
